32ad01b7899d45a5430ea7aab23bc8dc320fa934
oscarf4 <oscarvedo97@gmail.com>
2022-02-12 10:38:25 -0500

    Desafio de reduce en rangos de valores con acumulador inicializado en objeto vacío

--- reduce-reloaded/reduce-reloaded.js
+++ reduce-reloaded/reduce-reloaded.js
@@ -62,8 +62,26 @@ const data = [
 
   //Challenge rangos de números
 
-//   let list = [];
+  let list = [];
 
-//   for (let i = 0; i < 30; i++) {
-//       list.push(Math.random);
-//   }
+  for (let i = 0; i < 30; i++) {
+      list.push(Math.round(Math.random() * 10));
+  }
+
+  const rangos = list.reduce((obj, value) => {
+    if (value <= 5) {
+      obj['1-5'] += 1; 
+    } else if (value <= 8) {
+      obj['6-8'] += 1;
+    } else {
+      obj['9-10'] += 1;
+    }
+
+    return obj;
+  }, {
+    '1-5': 0,
+    '6-8': 0,
+    '9-10': 0
+  });
+
+  console.log(rangos);
